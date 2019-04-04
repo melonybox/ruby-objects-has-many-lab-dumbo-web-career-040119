fcfe232d1092815089c9216546c2d1d1b696ebae
Gino Desangles <blushingbox@yahoo.com>
2019-04-04 01:54:07 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Post
   attr_accessor :title, :author
   
@@ -16,4 +17,12 @@ class Post
     @author.instance_variable_get(:@name)
   end
   
+=======
+class Post
+  attr_accessor :name, :artist
+ 
+  def initialize(name)
+    @name = name
+  end
+>>>>>>> 325a7edb167a97c74da1e6db410c5865ef836cdd
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist
+<<<<<<< HEAD
   
   @@all = []
  
@@ -14,5 +15,10 @@ class Song
   
   def artist_name
     @artist.instance_variable_get(:@name)
+=======
+ 
+  def initialize(name)
+    @name = name
+>>>>>>> 325a7edb167a97c74da1e6db410c5865ef836cdd
   end
 end
