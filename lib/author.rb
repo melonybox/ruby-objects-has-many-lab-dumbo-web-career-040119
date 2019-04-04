012140bc1d430fd0ad8b5ad632b1722a9f6c0de0
Gino Desangles <blushingbox@yahoo.com>
2019-04-04 01:55:25 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -26,33 +26,4 @@ class Author
   def self.post_count
     @@count
   end
-=======
-class Author
-  attr_accessor :name
-  attr_reader :posts
-  
-  @@count = 0
- 
-  def initialize(name)
-    @name = name
-    @posts = []
-  end
-  
-  def add_post(posts)
-    @posts << posts
-    post.artist = self
-    @@count += 1
-  end
-  
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@count += 1
-  end
-  
-  def self.song_count
-    @@count
-  end
->>>>>>> 325a7edb167a97c74da1e6db410c5865ef836cdd
 end
